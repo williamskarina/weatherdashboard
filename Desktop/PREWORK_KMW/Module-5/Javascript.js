306dbf777c714c317c1e41c47e775ffda398f771
Karina Williams <coleman_karina@yahoo.com>
2021-07-29 21:04:31 -0700

    initial commit

---
+++ Desktop/PREWORK_KMW/Module-5/Javascript.js
@@ -0,0 +1,19 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function ChangeColor(){
+
+    document.getElementById("box").style.backgroundColor = 'Blue';
+
+});
+
+
+document.getElementById("button4").addEventListener("click", function ChangeColor(){
+    
+    document.getElementById("box").style.height = "150px"; 
+    document.getElementById("box").style.backgroundColor = 'Orange';
+
+});
---
+++ Desktop/scheduleplanner/script.js
@@ -0,0 +1,2 @@
+var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
