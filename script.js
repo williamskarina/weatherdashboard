21babdcfd7ffb1fbf4a122b82f480a134c7d127a
Karina Williams <coleman_karina@yahoo.com>
2021-08-05 22:08:28 -0700

    revised html

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 var APIKey = "efbadddaeba4c377be25ce82e79aa3ca";
 
 var city = "Houston";
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch(queryURL).then(function(response){
-    return response.json()
+ fetch(queryURL).then(function(response){
+     return response.json()
 }) .then(function(data){
-    console.log(data)
-})
+     console.log(data)
+ })
